b70f5991b4ca023d92b3c9afee9b93c12875b573
Thomas Lifuti <thomas@Thomass-MacBook-Pro.local>
2026-06-08 01:03:13 +0530

    Add initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260607_193246 from './20260607_193246';
+
+export const migrations = [
+  {
+    up: migration_20260607_193246.up,
+    down: migration_20260607_193246.down,
+    name: '20260607_193246'
+  },
+];
